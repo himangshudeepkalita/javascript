ad247e31a713a3f8bdb22bfc0581600a46170901
Himangshu Deep Kalita <106749078+himangshudeepkalita@users.noreply.github.com>
2024-05-26 19:12:19 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,9 +3,52 @@ let a = 300
 if(true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {
+    const username = "himangshu"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+
+// one();
+
+if(true) {
+    const username = "himangshu"
+    if(username === "himangshu") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting +++++++++++++++++++++
+
+
+console.log(addone(5));
+
+function addone(num) {
+    return num + 1
+}
+
+
+
+console.log(addTwo(5));
+const addTwo = function(num) {
+    return num + 2
+}
